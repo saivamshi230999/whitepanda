34582f4dbd7eaedc364728c4176f1d3acf621992
Subhadeep Dash <dashsubhadeep@gmail.com>
2019-09-30 22:03:44 +0530

    Updates

---
+++ precache-manifest.7ba7349f0fdacb72de18c090aa967905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88359ca665fafd5ece12804c6da45fa2",
+    "url": "/whitepanda/index.html"
+  },
+  {
+    "revision": "a0b580bf252c6de1f059",
+    "url": "/whitepanda/static/css/main.ab3c59bf.chunk.css"
+  },
+  {
+    "revision": "cd7dcc2b900f516c3b97",
+    "url": "/whitepanda/static/js/2.819bb253.chunk.js"
+  },
+  {
+    "revision": "a0b580bf252c6de1f059",
+    "url": "/whitepanda/static/js/main.15253eba.chunk.js"
+  },
+  {
+    "revision": "4e44ee84db63e6220d36",
+    "url": "/whitepanda/static/js/runtime-main.591c2cb8.js"
+  }
+]);
